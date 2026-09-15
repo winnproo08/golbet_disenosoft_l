18552e47d844489ccc1fbd4ba59baca7ede8b211
pc <pc@DESKTOP-ML85P18>
2026-09-15 17:33:40 -0500

    Add generic repository, match repository and database seeder (Module 3)

---
+++ GolBet.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using GolBet.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Repositories.Interfaces;
+
+/// <summary>
+/// Generic data-access contract for all domain entities.
+/// Specific queries live in entity-specific repositories.
+/// </summary>
+public interface IGenericRepository<T> where T : AuditableEntity
+{
+    // ---- Queries ---- 
+    Task<IEnumerable<T>> GetAllAsync(bool includeInactive = false);
+    Task<T?> GetByIdAsync(int id);
+
+    // ---- Commands ----
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeactivateAsync(int id);   // logical delete: IsActive = false
+}
+
---
+++ GolBet.Repositories/Interfaces/IMatchRepository.cs
@@ -0,0 +1,11 @@
+﻿// GolBet.Repositories/Interfaces/IMatchRepository.cs
+using GolBet.Entities;
+using GolBet.Entities.Enums;
+
+namespace GolBet.Repositories.Interfaces;
+
+public interface IMatchRepository : IGenericRepository<Match>
+{
+    Task<IEnumerable<Match>> GetAllWithTeamsAsync(MatchStatus? status = null);
+    Task<Match?> GetByIdWithDetailsAsync(int id);
+}
